8224bfd5ab876ec4aa2e632b75712888de5de9c6
Chase Engel <chase@Chases-MacBook-Pro.local>
2016-01-16 15:25:48 -0800

    Cleaned up.

--- dcanary.rb
+++ dcanary.rb
@@ -1,46 +1,54 @@
 #!/usr/bin/env ruby
 
-require "yaml"
-require "rest-client"
+require 'yaml'
+require 'rest-client'
 
 require_relative 'lib/locker.rb'
 require_relative 'lib/disk_helper.rb'
 
+config = YAML.load_file(File.dirname(__FILE__) + '/config.yml')
 
-config = YAML.load_file(File.dirname(__FILE__) + "/config.yml")
-
-config_variables = %w[telegram_api_token telegram_chat_id disks]
+config_variables = %w(telegram_api_token telegram_chat_id disks)
 
 # Check config file correctness.
 config_variables.each do |var|
-	if config[var].nil?
-		puts "[ERROR]: Please set '#{var}' in config file."
-		exit(1)
-	end
+  if config[var].nil?
+    puts "[ERROR]: Please set '#{var}' in config file."
+    exit(1)
+  end
 end
 
-locker = Locker.new config["disks"]
+# Convert limits to Fixnum.
+config['disks'].each_value(&:to_i)
 
-@token = config["telegram_api_token"]
-@chat_id = config["telegram_chat_id"]
+# Config shouldn't be changed
+config.freeze
+
+lkr = Locker.new config['disks']
+
+@token = config['telegram_api_token']
+@chat_id = config['telegram_chat_id']
 
 # Send a Telegram message
-def sendMessage disk
-	message = "#{DiskHelper.hostname} has disk #{disk} that is #{DiskHelper.percent(disk)}% full, #{DiskHelper.free(disk)} left."
-	url = "https://api.telegram.org/bot#{@token}/sendMessage"
-	RestClient.post url, {:chat_id => @chat_id, :text => message}
+def send_message(disk)
+  message = "#{DiskHelper.hostname} has disk #{disk}"
+  message << " that is #{DiskHelper.percent(disk)}% full,"
+  message << " #{DiskHelper.free(disk)} left."
+  url = "https://api.telegram.org/bot#{@token}/sendMessage"
+  RestClient.post url, chat_id: @chat_id, text: message
 end
 
 # Disk is over limit and unlocked.
 # Send message and lock that disk.
-config["disks"]
-	.select{ |disk, limit| DiskHelper.percent(disk) >= limit.to_i && !locker.locked?(disk) }
-	.each do |disk, limit|
-		sendMessage(disk)
-		locker.lock(disk)
-	end
+config['disks']
+  .select { |d, l| DiskHelper.percent(d) >= l && !lkr.locked?(d) }
+  .each do |d, _|
+    send_message(d)
+    lkr.lock(d)
+  end
 
 # Disk is under limit and locked.
 # Unlock that disk.
-config["disks"].select{ |disk, limit| DiskHelper.percent(disk) < limit.to_i && locker.locked?(disk) }
-	.each{ |disk, limit| locker.unlock(disk) }
+config['disks']
+  .select { |d, l| DiskHelper.percent(d) < l && lkr.locked?(d) }
+  .each { |d, _| lkr.unlock(d) }
